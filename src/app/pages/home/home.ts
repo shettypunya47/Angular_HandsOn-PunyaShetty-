ef4a2399b39b58173bd673b2a421028c28247280
Punya Shetty <shettypunya47@gmail.com>
2026-06-25 17:10:06 +0530

    Added Hands-On 2 Task 1 screenshot

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+// Property binding: component -> DOM
+// ngModel: two-way binding (DOM <-> component)
+  portalName = 'Student Course Portal';
+
+  isPortalActive = true;
+
+  message = '';
+
+  searchTerm = '';
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+
 }
