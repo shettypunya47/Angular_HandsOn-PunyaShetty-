bdbea99ce11fdc5adeef0e46aff586bfddaa23fd
Punya Shetty <shettypunya47@gmail.com>
2026-06-28 18:12:56 +0530

    Hands-On 2 Task 3

--- src/app/components/course-card/course-card.ts
+++ src/app/components/course-card/course-card.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
@@ -9,9 +9,21 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 })
 export class CourseCard implements OnChanges {
 
-  @Input() course: any;
+  @Input() course!: {
+    id: number,
+    name: string,
+    code: string,
+    credits: number,
+    instructor: string
+  };
+
+  @Output() enrollRequested = new EventEmitter<number>();
 
   ngOnChanges(changes: SimpleChanges) {
     console.log('Course changed:', changes['course']);
+  }
+
+  enroll() {
+    this.enrollRequested.emit(this.course.id);
   }
 }
--- src/app/pages/course-list/course-list.ts
+++ src/app/pages/course-list/course-list.ts
@@ -1,28 +1,58 @@
 import { Component } from '@angular/core';
-import { CourseCard } from '../../components/course-card/course-card';
 import { CommonModule } from '@angular/common';
+import { CourseCard } from '../../components/course-card/course-card';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [CourseCard, CommonModule],
+  imports: [CommonModule, CourseCard],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css'
 })
 export class CourseList {
 
+  selectedCourseId?: number;
+
   courses = [
     {
+      id: 1,
       name: 'Angular',
+      code: 'ANG101',
+      credits: 4,
       instructor: 'John'
     },
     {
+      id: 2,
       name: 'Java',
+      code: 'JAVA201',
+      credits: 3,
       instructor: 'Smith'
     },
     {
+      id: 3,
       name: 'Python',
+      code: 'PY301',
+      credits: 4,
       instructor: 'David'
+    },
+    {
+      id: 4,
+      name: 'DBMS',
+      code: 'DB401',
+      credits: 3,
+      instructor: 'Ravi'
+    },
+    {
+      id: 5,
+      name: 'OS',
+      code: 'OS501',
+      credits: 4,
+      instructor: 'Kumar'
     }
   ];
+
+  onEnroll(courseId: number) {
+    console.log('Enrolling in course:', courseId);
+    this.selectedCourseId = courseId;
+  }
 }
